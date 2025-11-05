4fdea53a00b78d29028836b56652d7bd6df366e0
purohatsu <dnwls010728@gmail.com>
2025-11-05 23:26:21 +0900

    FEAT: Add Empty Project

---
+++ Source/UE_GAS_Study/UE_GAS_Study.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_GAS_Study.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_GAS_Study, "UE_GAS_Study" );
